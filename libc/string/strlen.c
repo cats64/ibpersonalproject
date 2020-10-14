2fd6fc60c323fe5262c784dc34a46a05d089730e
Nathaniel Flores <nsf5280@outlook.com>
2020-10-14 16:32:46 -0600

    Add more verbose info

--- libc/string/strlen.c
+++ libc/string/strlen.c
@@ -1,10 +1,13 @@
 #include <string.h>
 
-/* This function returns the length of the string. */
+/* This function returns the length of the string. 
+ * Part of the <string.h> family of functions. 
+ * See the rest of the folder.*/
 size_t strlen(const char* str) {
     size_t len = 0;
     /* For every character in the string, increment the count by one */
     while (str[len])
         len++;
+    /* Then give back the result */
     return len;
 }
