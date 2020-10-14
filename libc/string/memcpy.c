5acc7188515b8c26d56d5343f2adf7c7323fd71c
Nathaniel Flores <nsf5280@outlook.com>
2020-10-14 16:54:03 -0600

    Explain some magic

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -3,9 +3,11 @@
 /* This function copies a string from one location to another. 
  * Part of the <string.h> family of functions. 
  * See the rest of the folder.*/
-void* memcpy(void* dstptr, const void* srcptr, size_t size) {
+void* memcpy(void* restrict dstptr, const void* restrict srcptr, size_t size) {
+    /* First create local variables of the arguments */
     unsigned char* dst = (unsigned char*) dsptr;
     const unsigned char* src = (const unsigned char*) srcptr;
+    /* Make sure that we only copy the number of bytes we're told to */'
     for (size_t i = 0; i < size; i++)
         dst[i] = src[i];
     return dstptr;
