15887c6c5540e3ef31959689e2096503dcc363d7
Nathaniel Flores <nsf5280@outlook.com>
2020-10-15 16:56:19 -0600

    Kernel improvement, make it work with boot.S

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,10 @@
-#include <stdint.h>
-#include "tty.h"
-
-void main(void) {
+#include <stdio.h>
+#include <kernel/tty.h>
+/* This kernel_main is called in the boot.S file.
+ * Not a lot is done here- it's mostly delegated somewhere else.
+ */
+void kernel_main(void) {
     term_init();
     // Newline support is still broken
-    term_writesector("Hello, world!");
+    printf("Print to screen test?\n");
 }
