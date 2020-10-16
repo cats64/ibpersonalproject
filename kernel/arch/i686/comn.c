ae0823f2cfefee83d475139fd16a57b7785748b0
Nathaniel Flores <nsf5280@outlook.com>
2020-10-15 21:37:34 -0600

    Create some simple assembly functions, put them in their own file

---
+++ kernel/arch/i686/comn.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stddef.h>
+
+#include <kernel/comn.h>
+
+// A function that sends a value on an I/O port.
+static inline void outb(uint16_t port, uint8_t val) {
+	asm volatile ( "outb %0, %1" 
+		       : "a"(val) 
+		       : "Nd"(port) );
+}
+
+// A function that reads a value on an I/O port.
+static inline uint8_t inb(uint16_t port) {
+	uint8_t ret;
+	asm volatile ( "inb %1, %0"
+		       : "=a"(ret)
+		       : "Nd"(port) );
+	return ret;
+}
+
+//TODO: add a poke() and peek() function to read regular memory
---
+++ kernel/include/kernel/comn.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+static inline void outb(uint16_t port, uint8_t val);
+static inline uint8_t inb(uint16_t port); 
