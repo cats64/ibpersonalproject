64870b05dbcf8a06ceee736d7b8d35b6c54b65c9
Nathaniel Flores <nsf5280@outlook.com>
2020-10-18 18:09:09 -0600

    Fix dstptr not being declared against dst

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -5,9 +5,9 @@
  * See the rest of the folder.*/
 void* memcpy(void* restrict dstptr, const void* restrict srcptr, size_t size) {
     /* First create local variables of the arguments */
-    unsigned char* dst = (unsigned char*) dsptr;
+    unsigned char* dst = (unsigned char*) dstptr;
     const unsigned char* src = (const unsigned char*) srcptr;
-    /* Make sure that we only copy the number of bytes we're told to */'
+    /* Make sure that we only copy the number of bytes we are told to */
     for (size_t i = 0; i < size; i++)
         dst[i] = src[i];
     return dstptr;
