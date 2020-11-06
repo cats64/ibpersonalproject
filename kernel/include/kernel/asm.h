0ab7f3bf19df3e6774f560da0eb31692c5f8a7c9
Nathaniel Flores <nsf5280@outlook.com>
2020-11-06 16:08:17 -0700

    Add header for asm.h, begin work on adding it to the build

---
+++ kernel/include/kernel/asm.h
@@ -0,0 +1,12 @@
+#ifndef _KERNEL_ASM_H
+#define _KERNEL_ASM_H
+
+#include <stdint.c>
+#include <stddef.h>
+
+// Function prototypes for asm.c
+static inline uint8_t inb(uint16_t);
+static inline void outb(uint16_t, uint8_t);
+
+#endif
+
