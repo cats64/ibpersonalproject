f21935aa9f93149b0ea3fe4589059c84dafd6885
Nathaniel Flores <nsf5280@outlook.com>
2020-11-17 21:23:31 -0700

    Improve build-package. It now works on major distros, Gentoo and OpenBSD.

--- build-packages.sh
+++ build-packages.sh
@@ -1,2 +1,39 @@
 #!/bin/sh
 set -e
+# build-packages.sh: install all distro-specific packages.
+getdistro() {
+    if [ -e /etc/redhat-release ]; then
+	export DISTRO="REDHAT"
+    elif [ -e /etc/fedora-release ]; then
+	export DISTRO="REDHAT"
+    elif [-e /etc/debian_release ]; then
+	export DISTRO="DEBIAN"
+    elif [-e /etc/gentoo-release ]; then
+	export DISTRO="GENTOO"
+    elif [ $(sysctl kern.version | cut -f 2 -d =) = "OpenBSD" ]; then
+	export DISTRO="OPENBSD"
+    fi
+}
+installpkg() {
+    if [ $DISTRO = "REDHAT" ]; then
+	sudo dnf install gcc gcc-c++ make bison flex gmp-devel libmpc-devel mpfr-devel texinfo
+    elif [ $DISTRO = "DEBIAN" ]; then
+	sudo apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libcloog-isl-dev libisl-dev
+    elif [ $DISTRO = "GENTOO" ]; then
+	sudo emerge --ask sys-devel/gcc sys-devel/make sys-devel/bison sys-devel/flex dev-libs/gmp dev-libs/mpc dev-libs/mpfr sys-apps/texinfo dev-libs/cloog dev-libs/isl
+    elif [ $DISTRO = "OPENBSD"]; then
+	doas pkg_add bison flex gmp libmpc mpfr texinfo
+    fi
+}
+
+getdistro
+while true; do
+    printf "Do you wish to install programs required for this build?\n[Y/N]:\t"
+    read yn
+    case $yn in
+	[Yy]* ) installpkg; break;;
+	[Nn]* ) exit;;
+	* ) echo "Please answer yes or no.";;
+    esac
+done
+printf "Now run build-tools.sh to install the rest of the toolchain.\n"
