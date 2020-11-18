21cf97232d168b1513d0e8168f740fbff677f963
Nathaniel Flores <nsf5280@outlook.com>
2020-11-17 21:54:54 -0700

    Add build-tools, is in a somewhat working state

--- build-packages.sh
+++ build-packages.sh
@@ -12,23 +12,28 @@ getdistro() {
 	export DISTRO="GENTOO"
     elif [ $(sysctl kern.version | cut -f 2 -d =) = "OpenBSD" ]; then
 	export DISTRO="OPENBSD"
+    else
+	printf "Your distro is unsupported. Look at README.org for further details.\n"
+	break
     fi
 }
 installpkg() {
     if [ $DISTRO = "REDHAT" ]; then
-	sudo dnf install gcc gcc-c++ make bison flex gmp-devel libmpc-devel mpfr-devel texinfo
+	sudo dnf install gcc gcc-c++ make bison flex gmp-devel libmpc-devel mpfr-devel texinfo grub2-tools-extra xorriso grub2
     elif [ $DISTRO = "DEBIAN" ]; then
-	sudo apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libcloog-isl-dev libisl-dev
+	sudo apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libcloog-isl-dev libisl-dev xorriso grub2
     elif [ $DISTRO = "GENTOO" ]; then
-	sudo emerge --ask sys-devel/gcc sys-devel/make sys-devel/bison sys-devel/flex dev-libs/gmp dev-libs/mpc dev-libs/mpfr sys-apps/texinfo dev-libs/cloog dev-libs/isl
+	sudo emerge --ask sys-devel/gcc sys-devel/make sys-devel/bison sys-devel/flex dev-libs/gmp dev-libs/mpc dev-libs/mpfr sys-apps/texinfo dev-libs/cloog dev-libs/isl sys-boot/grub:2 dev-libs/libisoburn
     elif [ $DISTRO = "OPENBSD"]; then
+	printf "You'll likely need to build xorriso yourself. YOU HAVE BEEN WARNED.\n"
 	doas pkg_add bison flex gmp libmpc mpfr texinfo
     fi
 }
 
+printf "Starting install...\n"
 getdistro
 while true; do
-    printf "Do you wish to install programs required for this build?\n[Y/N]:\t"
+    printf "Do you wish to install programs required for this build?\n[Y/N]: "
     read yn
     case $yn in
 	[Yy]* ) installpkg; break;;
--- build-tools.sh
+++ build-tools.sh
@@ -2,22 +2,37 @@
 set -e
 # build-tools.sh: a tool that creates the cross-compiler needed for building
 # Not done yet, DON'T RUN THIS
-export arch=i686-elf
-export prefix=$1
+export PREFIX="$HOME/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
 
-printf "Downloading GCC sources..."
-mkdir -p build/{build-gcc,build-binutils}
+printf "\nThis script is dangerous and being terminated for your safety.\n"
+break
+printf "Starting build..."
+mkdir -p $HOME/opt/cross $HOME/src/{build-gcc,build-binutils}
 # we download gcc 10.1 and binutils 2.34
-wget #Save gcc sources
-wget #Save binutils sources
-cd build/build-binutils
-tar xpvf *.xz
-./binutils-x.y.z/configure --disable-nls --with-sysroot --arch=$arch
-make
-make install
-cd ../build-gcc
-tar xpvf *.xz
-./gcc-x.y.z/configure --disable-nls --with-sysroot --arch=$arch
-make #figure this out
-make install
+buildbinutils() {
+    cd $HOME/src/build-binutils
+    wget https://ftp.gnu.org/gnu/binutils/binutils-2.35.tar.xz
+    tar xpvf binutils-2.35.tar.xz
+    ../binutils-2.35/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+    make
+    make install
+}
+
+buildgcc() {
+    cd $HOME/src/build-gcc
+    wget https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz
+    tar xpvf gcc-10.2.0.tar.xz
+    ../gcc-10.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+    make all-gcc
+    make all-target-libgcc
+    make install-gcc
+    make install-target-libgcc
+}
+
+printf "\n Building binutils...\n"
+buildbinutils
+printf "\n Building gcc...\n"
+buildgcc
 printf "Build complete, GCC and Binutils are ready for use."
